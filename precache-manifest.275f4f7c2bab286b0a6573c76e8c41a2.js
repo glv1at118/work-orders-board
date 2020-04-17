806929913931ebccf29410ccef896f321a53c5e3
Guannan <guannanlv@gmail.com>
2020-04-17 18:18:33 -0400

    Updates

---
+++ precache-manifest.275f4f7c2bab286b0a6573c76e8c41a2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27d5ebbcd41817560b8eb280d67dfa1d",
+    "url": "/work-orders-board/index.html"
+  },
+  {
+    "revision": "3a0f880d716a7d5a6ca7",
+    "url": "/work-orders-board/static/css/main.4fe127df.chunk.css"
+  },
+  {
+    "revision": "14eb988528e69e7a9362",
+    "url": "/work-orders-board/static/js/2.1d5e85e5.chunk.js"
+  },
+  {
+    "revision": "3a0f880d716a7d5a6ca7",
+    "url": "/work-orders-board/static/js/main.81d2a286.chunk.js"
+  },
+  {
+    "revision": "7cd938812df05edeb699",
+    "url": "/work-orders-board/static/js/runtime-main.1e50ee53.js"
+  }
+]);
